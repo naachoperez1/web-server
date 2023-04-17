461f6407eeab9d9b75296818bb3d0a987d8e05c3
Nacho <naachoperez11@gmail.com>
2023-04-17 13:00:11 -0300

    modified security

--- database.py
+++ database.py
@@ -1,15 +1,4 @@
 import sqlite3
-entries = []
-
-
-
-# def create_table():
-#     with connection:
-#         connection.execute("CREATE TABLE entries (content TEXT, date TEXT);")
-#
-#
-# def close_connection():
-#     connection.close()
 
 def add_entry(conexion,name, email,password):
     with conexion:
--- model.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-import psycopg2
-db = SQLAlchemy()
-
-class Usuario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    nombre = db.Column(db.String(50))
-    correo_electronico = db.Column(db.String(100), unique=True)
-    contrasena = db.Column(db.String(100))
