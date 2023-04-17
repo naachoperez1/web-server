35e45337cfc4d44d233dbfe699d0380bb2d68b50
Ignacio Perez <85687161+naachoperez1@users.noreply.github.com>
2023-04-17 17:35:24 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 import sqlite3
 
 def add_entry(conexion,name, email,password):
+    # Add entries from the user to the database.
     with conexion:
         cursor = conexion.cursor()
         cursor.execute("INSERT INTO users VALUES (?, ?, ?);", (name,email,password))
 
 
 def get_entries(conexion):
+    # Returns data from the database.
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM users")
     usuarios = cursor.fetchall()
