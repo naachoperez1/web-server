e8b6fbf646309add17c1f742c93d893dc30204aa
Ignacio Perez <85687161+naachoperez1@users.noreply.github.com>
2023-04-17 17:34:21 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 from werkzeug.security import generate_password_hash
-from model import db, Usuario
 import database
 import sqlite3
 import threading
 
 app = Flask(__name__)
 
+# Create connection with database.
 conexion = conn = sqlite3.connect("YOUR_DATABSE_LOCATION", check_same_thread=False)
 
 @app.route('/inicio')
@@ -15,15 +15,18 @@ def inicio():
 
 @app.route('/usuarios')
 def usuarios():
+    # Function that returns a cursor with all the users in the database.
     user = database.get_entries(conexion)
-    return render_template('usuarios.html', usuarios=user)
+    return render_template('usuarios.html', usuarios=user) # We pass 'user' as a parameter to the html so it shows it on the web page.
 
 @app.route('/registro', methods=['GET', 'POST'])
 def registro():
     if request.method == 'POST':
+        # Get the inputs from the form.
         nombre = request.form['nombre']
         correo_electronico = request.form['correo_electronico']
         contrasena = generate_password_hash(request.form['contrasena'])
+        # Add these inputs to the database.
         database.add_entry(conexion,nombre, correo_electronico, contrasena)
         return inicio()
 
